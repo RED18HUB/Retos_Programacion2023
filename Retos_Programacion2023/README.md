5fa547aa631684d29bd5a1101c9ff01bb950cf9a
Miguel Alexander Rojas Cobos <52639294+RED18HUB@users.noreply.github.com>
2023-01-22 01:10:31 -0500

    Reto 001 python

---
+++ Retos_Programacion2023/README.md
@@ -0,0 +1,2 @@
+# Retos_Programacion2023
+Se desarrollaran ejercicios de programación en distintos lenguajes de programación con la finalidad de mejorar la lógica de programación
---
+++ Retos_Programacion2023/RETO_001/python/buzz_fizz.py
@@ -0,0 +1,13 @@
+
+
+
+for dato in range(1,101):
+    if dato%15==0:
+        print('fizzbuzz')
+    elif dato%3==0:
+        print('fizz')
+    elif dato%5==0:
+        print('buzz')
+    else:
+        print(dato) 
+	
